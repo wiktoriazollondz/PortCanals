63191f7f6300869aebf896a65f3fcbec71ec484e
zoriaa <w.zollondz.037@studms.ug.edu.pl>
2026-04-26 16:57:31 +0200

    podpunkt 2 - ujednolicenie kroku czasowego, agregacja

--- dane.py
+++ dane.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import os
 
+#METEOROLOGICZNE
 meteo_files = [f for f in os.listdir('dane-2021-2025/dane-pogodowe-stacja-gora-gradowa-2021-2025') if f.endswith('.xlsx')]
+all_meteo = []
 
 for file in meteo_files:
     path = os.path.join('dane-2021-2025/dane-pogodowe-stacja-gora-gradowa-2021-2025', file)
@@ -16,25 +18,54 @@ for file in meteo_files:
         df[col] = df[col].astype(str).str.replace(',', '.')
         df[col] = pd.to_numeric(df[col], errors='coerce')
 
-    # opady
     df.loc[(df['Opad [mm]'] < 0) | (df['Opad [mm]'] > 100), 'Opad [mm]'] = np.nan
-
-    # temperatura
     df.loc[(df['Temperatura [C]'] < -30) | (df['Temperatura [C]'] > 40), 'Temperatura [C]'] = np.nan
-
-    # wilgotność
     df.loc[(df['Wilgotność [%]'] < 0) | (df['Wilgotność [%]'] > 100), 'Wilgotność [%]'] = np.nan
+    df.loc[(df['Ciśnienie [hPa]'] < 950) | (df['Ciśnienie [hPa]'] >= 1050), 'Ciśnienie [hPa]'] = np.nan
 
-    # ciśnienie
-    df.loc[(df['Ciśnienie [hPa]'] < 950) | (df['Ciśnienie [hPa]'] > 1050), 'Ciśnienie [hPa]'] = np.nan
+    df = df.dropna(subset=cols_to_fix)
 
-    # interpolacja danych - dodanie danej na podstawie sąsiednich
-    for col in cols_to_fix:
-        df[col] = df[col].interpolate(method='linear')
+    df["Data"] = pd.to_datetime(df["Data"])
+    df["Dzień"] = df["Data"].dt.normalize()
 
-    print(df.isnull().sum())
+    df_dobowe = df.groupby("Dzień").agg({
+        "Opad [mm]": "sum",
+        "Temperatura [C]": ["mean", "min", "max"],
+        "Wilgotność [%]": "mean",
+        "Ciśnienie [hPa]": "mean"
+    })
+    df_dobowe.columns = [
+        'Opad_suma',
+        'Temp_średnia', 'Temp_min', 'Temp_max',
+        'Wilgotność_średnia',
+        'Ciśnienie_średnia'
+    ]
+    all_meteo.append(df_dobowe)
 
+# połączenie wszystkich plików excel
+full_meteo = pd.concat(all_meteo).sort_index()
+# pełny kalendarz od początku do końca pomiarów
+full_range_meteo = pd.date_range(start=full_meteo.index.min(), end=full_meteo.index.max(), freq='D')
+full_meteo = full_meteo.reindex(full_range_meteo)
+full_meteo.index.name = 'Data'
+
+# kolumny pomocnicze
+full_meteo['month'] = full_meteo.index.month
+full_meteo['day'] = full_meteo.index.day
+
+# mediana dla każdego dnia w roku (np. mediana ze wszystkich 15 lipca)
+medians = full_meteo.groupby(['month', 'day']).transform('median')
+full_meteo = full_meteo.fillna(medians)
+full_meteo = full_meteo.drop(columns=['month', 'day'])  # usunięcie kolumn pomocniczych
+
+print("Ilość dni po poprawkach w danych meteorologicznych:", len(full_meteo))
+print(full_meteo.head())
+print(full_meteo.tail())
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#POZIOM WODY
 water_level_files = [f for f in os.listdir('dane-2021-2025/poziom-wody-ujscie-rzeki-strzyza-2021-2025') if f.endswith('.xlsx')]
+all_water_level = []
 
 for file in water_level_files:
     path = os.path.join('dane-2021-2025/poziom-wody-ujscie-rzeki-strzyza-2021-2025', file)
@@ -42,12 +73,40 @@ for file in water_level_files:
     df = df.dropna(how='all')
     df.columns = df.columns.str.strip()
 
-    # poziom wody
     df['Poziom wody [m]'] = df['Poziom wody [m]'].astype(str).str.replace(',', '.')
     df['Poziom wody [m]'] = pd.to_numeric(df['Poziom wody [m]'], errors='coerce')
 
-    # interpolacja danych - dodanie danej na podstawie sąsiednich
-    df['Poziom wody [m]'] = df['Poziom wody [m]'].interpolate(method='linear')
-    # df.loc[(df['Poziom wody [m]'] < -50) | (df['Poziom wody [m]'] > 100), 'Poziom wody [m]'] = np.nan
+    # df.loc[(df[nazwa_kolumny] < -10) | (df[nazwa_kolumny] > 10), nazwa_kolumny] = np.nan
 
-    print(df.isnull().sum())
+    df = df.dropna(subset=['Poziom wody [m]'])
+
+    df["Data"] = pd.to_datetime(df["Data"])
+    df["Dzień"] = df["Data"].dt.date
+
+    df_dobowe = df.groupby("Dzień").agg({
+        "Poziom wody [m]": ["mean", "max"],
+    })
+    df_dobowe.columns = [
+        'Poziom_wody_średnia', 'Poziom_wody_max'
+    ]
+    all_water_level.append(df_dobowe)
+
+# połączenie wszystkich plików excel
+full_water_level = pd.concat(all_water_level).sort_index()
+# pełny kalendarz od początku do końca pomiarów
+full_range_wl = pd.date_range(start=full_water_level.index.min(), end=full_water_level.index.max(), freq='D')
+full_water_level = full_water_level.reindex(full_range_wl)
+full_water_level.index.name = 'Data'
+
+# kolumny pomocnicze
+full_water_level['month'] = full_water_level.index.month
+full_water_level['day'] = full_water_level.index.day
+
+# mediana dla każdego dnia w roku (np. mediana ze wszystkich 15 lipca)
+medians = full_water_level.groupby(['month', 'day']).transform('median')
+full_water_level = full_water_level.fillna(medians)
+full_water_level = full_water_level.drop(columns=['month', 'day'])  # usunięcie kolumn pomocniczych
+
+print("Ilość dni po poprawkach w danych poziomu wody:", len(full_water_level))
+print(full_water_level.head())
+print(full_water_level.tail())
